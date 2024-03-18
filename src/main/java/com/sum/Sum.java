d8b3c4d595b05a5895a7cb02c615f11b6526807d
Your Name <somashekharsk226@gmail.com.com>
2024-03-18 21:13:16 +0530

    sdd

---
+++ src/main/java/com/sum/Sum.java
@@ -0,0 +1,18 @@
+package com.sum;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		int x=3;
+		if(x%2==0)
+		{
+			System.out.println("Even Number");
+		}
+		else
+		{
+			System.out.println("Odd Number");
+		}
+
+	}
+
+}
---
+++ src/test/java/com/sum/SumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
